323050ff840b37d1435a8548e4f505c635713b69
Indrakant Mishra <indrakantm23@gmail.com>
2021-10-06 17:25:54 +0530

    send mail

--- mailer/sendMail.js
+++ mailer/sendMail.js
@@ -10,8 +10,8 @@ const sendOnboardingMail = (data) => {
         secure: true,
         service: 'gmail',
         auth: {
-          user: process.env.EMAIL,
-          pass: process.env.PASSWORD
+          user: 'eventmaker23@gmail.com', //process.env.EMAIL,
+          pass: 'Vishal@123',//process.env.PASSWORD
         }
       });
 
@@ -35,14 +35,15 @@ const sendOnboardingMail = (data) => {
                         <th style="border: 1px solid #999;padding: 0.5rem;text-align: left;">Amount</th>
                     </tr>
                     ${data.data.map((el) => {
-                        return(`<tr>
+                        return(el && el.category? `<tr>
                             <td style="border: 1px solid #999; padding: 0.5rem;text-align: left;text-transform: capitalize;">${el.category}</td>
                             <td style="border: 1px solid #999; padding: 0.5rem;text-align: left;">${el.ticket_count}</td>
                             <td style="border: 1px solid #999; padding: 0.5rem;text-align: left;">₹${el.ticket_count * Number(el.amount)}</td>
-                        </tr>`)
+                        </tr>` : null)
                     })}
                     </table>
     `;
+    bookingData = bookingData.replace(/,/g, '');
 
       const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
       const DAYS   = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -64,7 +65,9 @@ const sendOnboardingMail = (data) => {
             from: '"Event Maker" <eventmaker23@gmail.com>',
             to: data.user.email,
             subject: 'Booking Confirmation',
-            html: 'Hello '+data.user.firstName+', <br><br>Your booking for '+data.eventData.eventName+' is confirmed. Below is details of your booking.<br><br>'+bookingData+'<br><br><b>Event location:</b> '+data.eventData.full_address+'<br><b>Timings: </b>Event will start on '+getEventDates(data.eventData.startDate, data.eventData.endDate)+' at '+ data.eventData.startTime +'<br><br>With Regards,<br>Team Eventmaker.<br><img style="width: 120px;" src="https://firebasestorage.googleapis.com/v0/b/eventsarounds.appspot.com/o/bucket%2Fevent-maker-logo1.png?alt=media&token=8fe6c6ef-9cda-487c-8434-5ca07dfbc79a"/>'
+            // html: 'Hello '+data.user.firstName+', <br><br>Your booking for '+data.eventData.eventName+' is confirmed. Below is details of your booking.<br><br>'+bookingData+'<br><br><b>Event location:</b> '+data.eventData.full_address+'<br><b>Timings: </b>Event will start on '+getEventDates(data.eventData.startDate, data.eventData.endDate)+' at '+ data.eventData.startTime +'<br><br>With Regards,<br>Team Eventmaker.<br><img style="width: 120px;" src="https://firebasestorage.googleapis.com/v0/b/eventsarounds.appspot.com/o/bucket%2Fevent-maker-logo1.png?alt=media&token=8fe6c6ef-9cda-487c-8434-5ca07dfbc79a"/>',
+            
+            html: '<div><div style="background: #06101f;height: 100px;overflow: hidden;"><img src="https://firebasestorage.googleapis.com/v0/b/eventsarounds.appspot.com/o/bucket%2Fevent-maker-logo1.png?alt=media&token=8fe6c6ef-9cda-487c-8434-5ca07dfbc79a" alt="Event Maker" style="width: 180px !important;margin: 0 auto !important;display: block !important;margin-top: 11px !important;"></div><div><p><b>Dear '+data.user.firstName+',</b> <br/><br/>Thank you for choosing Event Maker, your booking for '+data.eventData.eventName+' has been confirmed, please find your ticket below - </p></div><div style="min-height: 110px;border: 2px dashed #f1f3f4;margin-top: 30px;padding: 20px;margin-bottom: 25px;"><h1 style="font-size: 18px;text-transform: uppercase;font-family: sans-serif;word-break: break-word;color: #222;text-align: center;border-bottom: 1px solid #ddd;width: 310px;margin: 0 auto;padding-bottom: 5px;">'+data.eventData.eventName+'</h1><p style="text-align: center;margin: 10px;"><b>Location:</b> '+data.eventData.full_address+'</p><p style="text-align: center;margin: 10px;"><b>Timings:</b> Event will start on '+getEventDates(data.eventData.startDate, data.eventData.endDate)+' at '+ data.eventData.startTime +'</p></div>'+bookingData+'<div style="margin-top: 40px;"><p>Warm Regards,<br/>Event Maker</p></div></div>'
         };
 
         
